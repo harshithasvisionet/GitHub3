1af4cb5865e924c3417053c8fa5ffca8a1ce244c
harshithasvisionet <Harshitha.S@visionetsystems.com>
2022-01-14 10:06:16 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GitHub3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] oddnumber = new int[3];
+            oddnumber[0] = 1;
+            oddnumber[1] = 3;
+            oddnumber[2] = 5;
+            Console.WriteLine(oddnumber[1]);
+        }
+    }
+}
